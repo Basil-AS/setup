ea3e508d581f59d5440e3ca05c61eb3378f158ce
Vasilii <71924962+Basil-AS@users.noreply.github.com>
2023-12-30 19:50:05 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -6,12 +6,13 @@ set -e
 # Функция для установки необходимых пакетов
 install_packages() {
     sudo $1 update
-    sudo $1 install -y zsh git wget curl micro screenfetch
+    sudo $1 install -y zsh git wget micro screenfetch
 }
 
-# Функция для установки Oh My Zsh и плагинов
+# Установка Oh My Zsh и плагинов
 install_oh_my_zsh() {
-    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    export RUNZSH=no KEEP_ZSHRC=yes
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo history)/' ~/.zshrc
@@ -43,8 +44,7 @@ esac
 # Установка Oh My Zsh и плагинов
 install_oh_my_zsh
 
-# Применение изменений
-source ~/.zshrc
-
-# Сделать Zsh оболочкой по умолчанию
-chsh -s $(which zsh)
+# Смена оболочки на Zsh
+if [ "$SHELL" != "$(which zsh)" ]; then
+    chsh -s $(which zsh)
+fi
