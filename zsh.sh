f7423b5595bbcf5306c76df9d3645a2481e61e65
Vasilii <71924962+Basil-AS@users.noreply.github.com>
2024-01-03 20:08:20 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -69,7 +69,7 @@ interactive_mode() {
 
 # Проверка и установка пакетов
 install_packages() {
-    for package in zsh screenfetch htop; do
+    for package in zsh screenfetch htop neofetch util-linux-user; do
         echo "Устанавливаю $package..."
         install_package $package
     done
@@ -83,9 +83,5 @@ install_oh_my_zsh
 # Смена оболочки на Zsh
 if [ "$SHELL" != "$(which zsh)" ]; then
     echo "Меняю оболочку на Zsh..."
-    chsh -s $(which zsh)
+    chsh -s $(which zsh) || echo "Не удалось изменить оболочку. Пожалуйста, смените её вручную."
 fi
-
-# Удаление временных файлов
-echo "Удаляю временные файлы..."
-rm -rf /tmp/* || true
