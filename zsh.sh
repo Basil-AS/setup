d4cb6e4de0be44fce32c8f3af7578503fe83316b
Vasilii <71924962+Basil-AS@users.noreply.github.com>
2024-01-03 18:00:14 +0300

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,51 +1,83 @@
 #!/bin/bash
 
-# Завершить скрипт при любой ошибке
-set -e
+# Логирование
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
 
-# Функция для установки необходимых пакетов
+# Функция проверки наличия необходимых утилит
+check_required_tools() {
+    for tool in curl git wget micro; do
+        if ! command -v $tool &> /dev/null; then
+            echo "Требуется $tool, но он не установлен. Устанавливаю..."
+            install_package $tool
+        fi
+    done
+}
+
+# Функция установки пакетов
+install_package() {
+    if [[ -x "$(command -v apt)" ]]; then
+        sudo apt install -y $1
+    elif [[ -x "$(command -v dnf)" ]]; then
+        sudo dnf install -y $1
+    elif [[ -x "$(command -v zypper)" ]]; then
+        sudo zypper install -y $1
+    elif [[ -x "$(command -v pacman)" ]]; then
+        sudo pacman -S --noconfirm $1
+    elif [[ -x "$(command -v apk)" ]]; then
+        sudo apk add $1
+    else
+        echo "Не удалось найти подходящий пакетный менеджер."
+        exit 1
+    fi
+}
+
+# Проверка и установка пакетов
 install_packages() {
-    sudo $1 update -y
-    sudo $1 install -y zsh git wget micro screenfetch
+    for package in zsh git wget micro screenfetch htop neofetch; do
+        echo "Устанавливаю $package..."
+        install_package $package
+        if [ $? -ne 0 ]; then
+            echo "Ошибка при установке $package"
+            exit 1
+        fi
+    done
 }
 
 # Установка Oh My Zsh без интерактивных запросов
 install_oh_my_zsh() {
+    echo "Устанавливаю Oh My Zsh..."
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+    if [ $? -ne 0 ]; then
+        echo "Ошибка при установке Oh My Zsh"
+        exit 1
+    fi
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-    # Изменение .zshrc с использованием sed
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting sudo history)/' ~/.zshrc
     sed -i 's/ZSH_THEME=".*"/ZSH_THEME="af-magic"/' ~/.zshrc
 }
 
-# Определение операционной системы и установка пакетов
-OS=$(grep ^ID= /etc/os-release | cut -d'=' -f2 | tr -d '"')
+# Интерактивный режим
+interactive_mode() {
+    read -p "Хотите установить дополнительные утилиты? (y/n) " answer
+    if [ "$answer" != "${answer#[Yy]}" ]; then
+        install_packages
+    fi
+}
 
-case $OS in
-    ubuntu|debian)
-        install_packages "apt"
-        ;;
-    fedora)
-        install_packages "dnf"
-        ;;
-    centos)
-        install_packages "yum"
-        ;;
-    opensuse-leap|opensuse-tumbleweed)
-        install_packages "zypper"
-        ;;
-    *)
-        echo "Операционная система не поддерживается."
-        exit 1
-        ;;
-esac
-
-# Установка Oh My Zsh
+# Основная логика скрипта
+check_required_tools
+interactive_mode
 install_oh_my_zsh
 
-# Попытка смены оболочки на Zsh
+# Смена оболочки на Zsh
 if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Меняю оболочку на Zsh..."
     chsh -s $(which zsh)
 fi
+
+# Удаление временных файлов
+echo "Удаляю временные файлы..."
+rm -rf /tmp/* || true
